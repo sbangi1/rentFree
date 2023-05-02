5325c0dd8389ea1b627af89cbe6f813b2dfcc01b
BlakeCaraballo <94406324+BlakeCaraballo@users.noreply.github.com>
2023-05-02 18:14:10 -0400

    Add files via upload

---
+++ buyer_dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buyer Dashboard</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="seller.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<p>Under Construction!</p>
+</body>
+</html>
